56b4d1d5e7fe496a0b71a006103fb9f95a94fceb
87labs <gustavorosawinter@gmail.com>
2025-03-22 00:53:55 -0300

    add backgrounds parameters

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,22 @@
-import type { Preview } from '@storybook/web-components'
+import type { Preview } from "@storybook/web-components";
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: "light",
+      values: [
+        { name: "light", value: "#ffffff" },
+        { name: "dark", value: "#000000" },
+        { name: "gray", value: "#f0f0f0" },
+        { name: "gray-dark", value: "#343a40" },
+        { name: "gray-light", value: "#f8f9fa" },
+        { name: "transparent", value: "transparent" },
+      ],
+    },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
