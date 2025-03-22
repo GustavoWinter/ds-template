eba199704535584b94cd8345b6c59539b19f93af
87labs <gustavorosawinter@gmail.com>
2025-03-22 01:28:11 -0300

    add decorator to table story

--- src/stories/tables/Table.stories.ts
+++ src/stories/tables/Table.stories.ts
@@ -3,11 +3,13 @@ import type { Meta, StoryObj } from "@storybook/web-components";
 import type { TableProps } from "./Table";
 import { Table } from "./Table";
 import { NavLinkProps } from "../typography/NavLink";
+import { html } from "lit";
 
 const meta = {
   component: "Table",
   tags: ["autodocs"],
   render: (args) => Table(args),
+  decorators: [(Story) => html`<div class="py-5 px-12">${Story()}</div>`],
 } satisfies Meta<TableProps>;
 
 export default meta;
